b196cbc9f526999c8e54d0c09760cd2718595cef
Vanishary <505718579@qq.com>
2022-03-31 10:17:50 +0800

    第一个带Controller和RequestMapping注解的SpringMVC程序

--- chapter11/src/main/java/com/ssm/controller/ControllerTest.java
+++ chapter11/src/main/java/com/ssm/controller/ControllerTest.java
@@ -1,7 +1,9 @@
 package com.ssm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,15 +13,24 @@ import javax.servlet.http.HttpServletResponse;
  * @description: This is a description
  * @create: 2022年03月30日 16:04
  */
-public class ControllerTest implements Controller {
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        //创建ModelAndView对象
-        ModelAndView modelAndView = new ModelAndView();
-        //向模型对象中添加一个名称为msg的字符串对象
-        modelAndView.addObject("msg","第一个Spring MVC程序");
-        //设置返回的试图路径
-        modelAndView.setViewName("/WEB-INF/jsp/welcome.jsp");
-        return modelAndView;
+@Controller
+@RequestMapping(value = "/controll")
+public class ControllerTest {
+    @RequestMapping(value = "/annotationController")
+    public String handleRequest(HttpServletRequest arg0, HttpServletResponse arg1, Model model) throws Exception {
+        model.addAttribute("msg", "第一个带注解的Spring MVC程序");
+        return "welcome";
     }
 }
+//public class ControllerTest implements Controller {
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+//        //创建ModelAndView对象
+//        ModelAndView modelAndView = new ModelAndView();
+//        //向模型对象中添加一个名称为msg的字符串对象
+//        modelAndView.addObject("msg","第一个Spring MVC程序");
+//        //设置返回的试图路径
+//        modelAndView.setViewName("/WEB-INF/jsp/welcome.jsp");
+//        return modelAndView;
+//    }
+//}
